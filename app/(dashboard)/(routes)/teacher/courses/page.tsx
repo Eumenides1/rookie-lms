bb88d2bb09cd2197eab7e0e54884e8e06ce9590e
ashbur <18829526908@163.com>
2024-10-30 23:18:21 +0800

    feat: 引入 shadcn form组件与 hot toast

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
     return (
-        <div>
-            CoursesPage
+        <div className="p-6">
+            <Link href="/teacher/create">
+                <Button>
+                    创建课程
+                </Button>
+            </Link>
         </div>
     );
 }
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { Form, 
+    FormControl, 
+    FormDescription, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage 
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "标题不能为空"
+    })
+});
+
+const CreatePage = () => {
+    const router = useRouter();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: ""
+        }
+    });
+    const { isSubmitting, isValid } = form.formState;
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post("/api/course", values)
+            router.push(`/teacher/courses/${response.data.id}`)
+        }catch {
+            toast.error("请求出错，请稍后重试！");
+        }
+    }
+    return (
+        <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+            <div>
+                <h1 className="text-2xl font-bold">
+                    课程名称
+                </h1>
+                <p className="text-sm text-slate-600">
+                    请输入课程名称
+                </p>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    >
+                        <FormField 
+                            control={form.control}
+                            name="title"
+                            render={({ field })=>(
+                                <FormItem>
+                                    <FormLabel>
+                                        课程标题
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Java开发入门'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        给你的课程起一个高大酷炫的名字吧！
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Link href='/'>
+                                <Button
+                                    type="button"
+                                    variant='ghost'
+                                >
+                                    关闭
+                                </Button>
+                            </Link>
+                            <Button type="submit" disabled={!isValid || isSubmitting}>
+                                下一步
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </div>
+    );
+}
+
+export default CreatePage;
