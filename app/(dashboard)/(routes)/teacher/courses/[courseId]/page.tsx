d50e1207eaa8477ac38060916bc3a1df30c0e33d
ashbur <18829526908@163.com>
2024-10-31 22:25:07 +0800

    feat: Next15之后动态 API（如 params 和 searchParams）已被异步化

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,10 +1,6 @@
-// 将 CourseIdPage 定义为服务器端组件
-export default async function CourseIdPage({ params }: { params: { courseId: string } }) {
-    const { courseId } = await params.courseId;
-    
-    return (
-        <div>
-            CourseId: {courseId}
-        </div>
-    );
+async function CourseIdPage({ params }: { params: { courseId: string } }) {
+    const { courseId } = await params;
+    return <p>课程 ID: {courseId}</p>;
 }
+
+export default CourseIdPage;
