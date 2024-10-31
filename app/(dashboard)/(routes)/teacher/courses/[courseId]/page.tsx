8e462658b8d29b49eb49748cb55a566acec18d1e
ashbur <18829526908@163.com>
2024-10-31 22:27:31 +0800

    fix: 修复React 未显示引入 Bug

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 async function CourseIdPage({ params }: { params: { courseId: string } }) {
     const { courseId } = await params;
     return <p>课程 ID: {courseId}</p>;
